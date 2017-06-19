8e7939a9a296e40d7849feb9071fd2962a173a17
shilong.sang <i5ting@126.com>
2017-06-19 21:29:25 +0800

    lazycommmit, please ignore

---
+++ app/src/main/java/com/i5ting/sang/a1047/MainActivity.java
@@ -0,0 +1,87 @@
+package com.i5ting.sang.a1047;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.graphics.PixelFormat;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.tencent.smtt.sdk.QbSdk;
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    com.tencent.smtt.sdk.WebView tbsContent;
+    private String url = "http://3g.nxtv.cn/fm/?chn=1047";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFormat(PixelFormat.TRANSLUCENT);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE | WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        initView();
+    }
+
+    private void initView() {
+        tbsContent = (com.tencent.smtt.sdk.WebView)findViewById(R.id.tbsContent);
+        tbsContent.loadUrl(url);
+        WebSettings webSettings = tbsContent.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        tbsContent.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && tbsContent.canGoBack()) {
+            tbsContent.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+          //  return true;
+       // }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
